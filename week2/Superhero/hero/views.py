39f728b52e278f2a89935b2ed92c40acf6256356
melsanch <sanc5365@bears.unco.edu>
2021-09-03 13:21:05 -0600

    Hero app creation

---
+++ week2/Superhero/hero/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import TemplateView
+
+
+class HulkView(TemplateView):
+    template_name = 'hulk.html'
+    
+    
+ 
+class IronManView(TemplateView):
+    template_name = "iron_man.html"
+    
+    
+
+class BlackWidowView(TemplateView):
+    template_name = 'black_widow.html'
