cdba6346b4755c8e6a2f99894d1c157f453abf8f
melsanch <sanc5365@bears.unco.edu>
2021-09-10 13:56:46 -0600

    superhero app

--- week3/Superhero/config/urls.py
+++ week3/Superhero/config/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from Hero.views import HulkView, IronManView, BlackWidowView
+from Hero.views import IndexView, HulkView, IronManView, BlackWidowView
 
 urlpatterns = [
+    path('', IndexView.as_view()),
     path('hulk', HulkView.as_view()),
     path('ironman',IronManView.as_view()),
     path('blackwidow', BlackWidowView.as_view()),
