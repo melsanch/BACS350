dc1eedefedff034e1d0d2214a6131eda4948bb2d
melsanch <sanc5365@bears.unco.edu>
2021-09-17 13:58:08 -0600

    superhero app changes

--- week4/Superhero/config/urls.py
+++ week4/Superhero/config/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from Hero.views import IndexView
+from Hero.views import IndexView, HeroListView
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view()),
-    
+    path('hero/', HeroListView.as_view()),
 ]
