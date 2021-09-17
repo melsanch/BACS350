d3ae4cfc84de1a649fa6c4909ed4d7687f3ab0e1
melsanch <sanc5365@bears.unco.edu>
2021-09-17 13:58:24 -0600

    superhero app changes

--- week4/Superhero/Hero/views.py
+++ week4/Superhero/Hero/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+from .models import Hero
+
+class HeroListView(ListView):
+    model = Hero
+    template_name = 'hero_list.html'
+
+class HeroListView(ListView):
+    model = Hero
+    template_name = 'hero_list.html'
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-
