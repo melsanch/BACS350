0188bb73156118760b50e2a6943c6d649bf0e930
melsanch <sanc5365@bears.unco.edu>
2021-09-22 17:52:33 -0600

    superhero app changes

--- week5/Superhero/hero/models.py
+++ week5/Superhero/hero/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Superhero(models.Model):
     name = models.CharField(max_length=20)
     identity = models.CharField(max_length=20)
+    image = models.CharField(max_length=200)
     
     def __str__(self):
         return f'{self.name}'
