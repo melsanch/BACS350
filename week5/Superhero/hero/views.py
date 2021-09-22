f8f4051040eebeca86b8338a421728c09f91dde3
melsanch <sanc5365@bears.unco.edu>
2021-09-22 17:51:05 -0600

    superhero app changes

--- week5/Superhero/hero/views.py
+++ week5/Superhero/hero/views.py
@@ -10,13 +10,17 @@ class HeroListView(ListView):
     model = Superhero
     template_name = 'hero_list.html'
 
-class HeroDetailView(DetailView):
+class HeroDetailView(TemplateView):
     model = Superhero
     template_name = 'hero_detail.html'
 
     def get_context_data(self, **kwargs):
         hero_id = kwargs['pk']
         hero = Superhero.objects.get(pk=hero_id)
-        return {'hero': hero}
+
+        
+        return {'hero': hero
+                
+        }
 
     
