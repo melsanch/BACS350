58a59de810c3c556c618804bf132afe1c0b8295b
melsanch <sanc5365@bears.unco.edu>
2021-10-04 12:47:20 -0600

    superhero app changes

--- week6/Superhero/hero/views.py
+++ week6/Superhero/hero/views.py
@@ -1,3 +1,5 @@
+from django.views.generic import DetailView, ListView, TemplateView
+from .models import Superhero
 from django.shortcuts import render
 
 class IndexView(TemplateView):
@@ -7,3 +9,16 @@ class IndexView(TemplateView):
 class HeroListView(ListView):
     model = Superhero
     template_name = 'hero_list.html'
+
+class HeroDetailView(TemplateView):
+    model = Superhero
+    template_name = 'hero_detail.html'
+
+    def get_context_data(self, **kwargs):
+        hero_id = kwargs['pk']
+        hero = Superhero.objects.get(pk=hero_id)
+
+        
+        return {'hero': hero
+                
+        }
