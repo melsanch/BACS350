bcd3bbaddf48138220e159650dcd4f6de08c7ec8
melsanch <sanc5365@bears.unco.edu>
2021-10-27 11:41:38 -0600

    Superhero app changes

---
+++ week9/Superhero/Documents/Hero.md
@@ -0,0 +1,15 @@
+# A Hero's Guide
+
+by mel
+
+----
+
+Theres a whole bunch of heros everywhere
+
+## A Sample of Heros
+
+* Black Widow
+* Iron Man
+* Hulk
+* Captain America
+* Scarlet Witch
--- week9/Superhero/doc/views.py
+++ week9/Superhero/doc/views.py
@@ -5,6 +5,6 @@ class DocumentView(TemplateView):
     template_name = 'document.html'
 
     def get_context_data(self, **kwargs):
-        document = self.kwargs.get('doc', 'Index')
+        document = 'Hero'
         markdown_text = open(f'Documents/{document}.md').read()
         return dict(doc=markdown(markdown_text), file=document)
