a3d3b24daa3d5cb9ef5b5fbe75b2bbfa684e256a
melsanch <sanc5365@bears.unco.edu>
2021-10-27 13:33:12 -0600

    superhero app changes

--- week9/Superhero/hero/admin.py
+++ week9/Superhero/hero/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Superhero
+from .models import Article, Superhero
 
 admin.site.register(Superhero)
+admin.site.register(Article)
 # Register your models here.
